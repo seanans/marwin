7ada6321b0f2744894c8fce7c0f9e4ca3bd485a8
seanans <maks1998ivan@gmail.com>
2023-11-30 16:46:12 +0200

    Swagger added

--- src/main/java/com/marwin/customerservice/CustomerserviceApplication.java
+++ src/main/java/com/marwin/customerservice/CustomerserviceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.marwin.customerservice", "com.marwin.customerservice.mappers"})
+@ComponentScan(basePackages = {"com.marwin.customerservice", "com.marwin.customerservice.mappers", "com.marwin.customerservice.controller"})
 public class CustomerserviceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/marwin/customerservice/controller/CustomerController.java
+++ src/main/java/com/marwin/customerservice/controller/CustomerController.java
@@ -1,29 +1,28 @@
 package com.marwin.customerservice.controller;
 
 import com.marwin.customerservice.models.CreateCustomerDTO;
+import com.marwin.customerservice.models.CustomerDTO;
 import com.marwin.customerservice.models.WelcomeResponse;
 import com.marwin.customerservice.services.CustomerService;
 import com.marwin.customerservice.shared.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("v1/customer")
+
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
 
+
     @PostMapping("/create")
     //private ResponseEntity<ApiResponse<WelcomeResponse>> createCustomer(@RequestBody CreateCustomerDTO customerDTO) {
     //return customerService.createCustomer(customerDTO);
-    public ResponseEntity<?> createCustomer(@RequestBody CreateCustomerDTO customerDTO) {
+    private ResponseEntity<?> createCustomer(@RequestBody CreateCustomerDTO customerDTO) {
         ApiResponse<WelcomeResponse> response = customerService.createCustomer(customerDTO).getBody();
 
         assert response != null;
